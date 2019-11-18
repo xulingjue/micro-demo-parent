2bf408cd972487081cbd4ee6dc92432c1ff2b861
许凌绝 <lingjue.xu@qq.com>
2019-11-18 17:21:07 +0800

    demo start success on local k8s

--- gateway-svc/src/main/java/xyz/micro_demo/gateway/GatewayApplication.java
+++ gateway-svc/src/main/java/xyz/micro_demo/gateway/GatewayApplication.java
@@ -27,14 +27,14 @@ public class GatewayApplication {
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 //增加一个path匹配，以"/customize/hello/"开头的请求都在此路由
-                .route(r -> r.path("/customize/hello/**")
+                .route(r -> r.path("/account/**")
                         //表示将路径中的第一级参数删除，用剩下的路径与webdemo的路径做拼接，
                         //这里就是"lb://webdemo/hello/"，能匹配到webdemo的HelloController的路径
                         .filters(f -> f.stripPrefix(1)
                                 //在请求的header中添加一个key&value
                                 .addRequestHeader("extendtag", "geteway-" + System.currentTimeMillis()))
                         //指定匹配服务webdemo，lb是load balance的意思
-                        .uri("lb://webdemo")
+                        .uri("lb://account-service")
                 ).build();
     }
 }
